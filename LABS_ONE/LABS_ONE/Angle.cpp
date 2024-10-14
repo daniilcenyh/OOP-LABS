e19c39742ece7b8efa818c67803bfd65f3935c0d
daniil <daniilcernyh2006@gmail.com>
2024-10-14 22:03:50 +0300

    правки первой лабы

--- LABS_ONE/LABS_ONE/Angle.cpp
+++ LABS_ONE/LABS_ONE/Angle.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "Angle.h"
 #include <math.h>
+#include <string>
 
-double Angle::conversion_to_radians(double _degrees, double _minutes) 
+
+std::string Angle::to_string()
+{
+	return "degreess: " + std::to_string(degrees) + " " + "minutes: " + std::to_string(minutes);
+}
+
+double Angle::conversion_to_radians(double _degrees, double _minutes)
 {
 	double result_in_radians;
 	double minutes_in_degrees = _minutes / 60;
@@ -12,47 +19,47 @@ double Angle::conversion_to_radians(double _degrees, double _minutes)
 	return result_in_radians;
 }
 
-int Angle::normalize_360(double _degrees, double _minutes)
-{	
-	double minutes_in_degrees = _minutes / 60;
-	int angle = _degrees + minutes_in_degrees;
-
-	angle = angle % 360;
-
-	if (angle < 0) angle += 360;
-
-	return angle;
+void Angle::normalize_360()
+{
+	degrees = (degrees + (minutes / 60)) % 360;
+	if (degrees < 0) degrees += 360;
+	minutes = 0;
 }
 
-double Angle::increasing_angle_by_given_amount(double _degrees, double _minutes, double _amount)
+
+void Angle::reducing_angle_by_given_amount(Angle other_angle)
 {
-	double result = _degrees;
-	double minutes_in_degrees = _minutes / 60;
-	result += minutes_in_degrees;
+	int other_angle_in_degrees = other_angle.get_minutes() / 60 + other_angle.get_degrees();
 
-	result += _amount;
-
-	return result;
-
+	degrees -= other_angle_in_degrees;
 }
 
-double Angle::reducing_angle_by_given_amount(double _degrees, double _minutes, double _amount)
+double Angle::sin_angle()
 {
-	double result = _degrees;
-	double minutes_in_degrees = _minutes / 60;
-	result += minutes_in_degrees;
-
-	result -= _amount;
-
-	return result;
-}
-
-double Angle::sinus_angle(double _degrees, double _minutes)
-{
-	double minutes_in_degrees = _minutes / 60;
-	double result = _degrees + minutes_in_degrees;
+	double result = degrees + minutes / 60;
 
 	return std::sin(result);
-
 }
+
+bool Angle::comparing_two_angles(Angle other_angle)
+{
+	double degrees_other_angle = other_angle.get_degrees() + other_angle.get_minutes() / 60;
+	double degrees_original_angle = degrees + minutes / 60;
+
+	if (degrees_original_angle == degrees_other_angle) {
+		return true;
+	}
+
+	return false;
+}
+
+void Angle::increasing_angle_by_given_amount(Angle other_angle)
+{
+	int other_angle_in_degrees = other_angle.get_minutes() / 60 + other_angle.get_degrees();
+	
+	degrees += other_angle_in_degrees;
+}
+
+
+
 
--- LABS_ONE/LABS_ONE/Angle.h
+++ LABS_ONE/LABS_ONE/Angle.h
@@ -1,48 +1,53 @@
 #pragma once
 #include <iostream>
 #include <cmath>
+#include <string>
+//using std::string;
 
 
 class Angle {
-	double degrees;
-	double minutes;
+
+	int degrees;
+	int minutes;
 	const double one_degrees_in_radian = std::acos(-1.0) / 180; // константа - 1 градус в радианах
+
 public:
 	// конструктор без параметров
 	Angle() {
-		this->degrees = 0;
-		this->minutes = 0;
+		degrees = 0;
+		minutes = 0;
 	}
 	// конструктор задающий величину переменным degrees, minutes
-	Angle(double _degrees, double _minutes) {
-		this->degrees = _degrees;
-		this->minutes = _minutes;
+	Angle(int _degrees, int _minutes) {
+		degrees = _degrees;
+		minutes = _minutes;
 	}
 
 	int get_degrees() {
-		return this->degrees;
+		return degrees;
 	}
 	int get_minutes() {
-		return this->minutes;
+		return minutes;
 	}
-	void set_degrees(double _degrees) {
-		this->degrees = _degrees;
+	void set_degrees(int _degrees) {
+		degrees = _degrees;
 	}
-	void set_minutes(double _minutes) {
-		this->minutes = _minutes;
+	void set_minutes(int _minutes) {
+		minutes = _minutes;
 	}
-	void to_string() const{
-		std::cout << "degrees: " << this->degrees << "\nminutes: " << this->minutes;
-	}
+	std::string to_string();
 
 	double conversion_to_radians(double _degrees, double _minutes);
 
-	int normalize_360(double _degrees, double _minutes);
+	void normalize_360();
 
-	double increasing_angle_by_given_amount(double _degrees, double _minutes, double _amount);
+	void reducing_angle_by_given_amount(Angle other_angle);
 
-	double reducing_angle_by_given_amount(double _degrees, double _minutes, double _amount);
+	double sin_angle();
 
+	bool comparing_two_angles(Angle other_angle);
 
+	void increasing_angle_by_given_amount(Angle other_angle);
 
+	~Angle() = default;
 };
